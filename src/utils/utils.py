58b564bcd48cb2b28a28bb1e2af1dfdea5d29240
marcodmc <marcodamascenodev@gmail.com>
2024-12-03 08:29:39 -0300

    feat: added function to delete .wdm folder and function to modify path to chromedriver

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,15 +1,20 @@
-import time, pyautogui , random, re
+import time, pyautogui , random, re, shutil, os, stat, platform, json
 from pathlib import Path
+
+
 def Sleeping(timer):
     return time.sleep(timer)
 
 def CreateFolder(name):
-    folder = Path(f"C:/{name}")
+    if os.name == 'nt':  # Windows
+        folder = Path("C:/") / name
+    else:  # Linux ou Mac
+        folder = Path.home() / "Downloads" / name
     if not folder.exists():
-        folder.mkdir(parents=True) 
-        print(f"Folder '{folder}' created successfully!")
+        folder.mkdir(parents=True)
+        print(f"Folder '{folder}' pasta download criada com sucesso!")
     else:
-        return
+        print(f"a pasta '{folder}' já existe.")
     
 def PressKeyTimes(x):
     count = 0
@@ -85,3 +90,62 @@ def ValidateDate(date):
     if len(DATE) > 8 or len(DATE) < 8:
         return False
     return True
+
+def DeleteDriverFolder():
+    full_path = os.path.join(os.path.expanduser("~"), ".wdm")
+
+    def HandleRemoveReadonly(exc_type, exc_value, exc_traceback):
+        if isinstance(exc_value, PermissionError):
+            path = exc_value.filename
+            os.chmod(path, stat.S_IWRITE)
+            os.remove(path)
+        else:
+            raise exc_value
+
+    try:
+        shutil.rmtree(full_path, onexc=HandleRemoveReadonly)
+        print(f"Pasta '{full_path}' removida com sucesso!")
+    except FileNotFoundError:
+        print(f"A pasta '{full_path}' não existe.")
+    except PermissionError:
+        print(f"Permissão negada para remover a pasta '{full_path}'.")
+    except Exception as e:
+        print(f"Ocorreu um erro ao tentar remover a pasta: {e}")
+
+def ModifyChromedriverPath():
+    user_home = os.path.expanduser("~")
+    wdm_dir = os.path.join(user_home, ".wdm")
+
+    drivers_json_path = os.path.join(wdm_dir, "drivers.json")
+
+    if not os.path.exists(drivers_json_path):
+        print(f"Arquivo {drivers_json_path} não encontrado.")
+        return
+
+    with open(drivers_json_path, "r", encoding="utf-8") as file:
+        drivers_data = json.load(file)
+
+    modified = False
+    for driver in drivers_data.get('drivers', []):
+        if driver.get('name') == 'chromedriver':
+            old_path = driver.get('binary_path', '')
+            new_path = old_path.replace('THIRD_PARTY_NOTICES.chromedriver', 'chromedriver')
+            driver['binary_path'] = new_path
+            print(f"Caminho do driver modificado de {old_path} para {new_path}")
+            modified = True
+
+    if modified:
+        with open(drivers_json_path, "w", encoding="utf-8") as file:
+            json.dump(drivers_data, file, indent=4)
+
+        if platform.system() != 'Windows':
+            try:
+                if os.path.exists(new_path):
+                    os.chmod(new_path, 0o755)
+                    print(f"Permissões de execução definidas para {new_path}")
+                else:
+                    print(f"Caminho {new_path} não encontrado para ajustar permissões.")
+            except Exception as e:
+                print(f"Erro ao definir permissões: {e}")
+    else:
+        print("Nenhuma modificação foi necessária no arquivo JSON.")
