d3d913a7ffdc81e21241fa6dd5fb56f3725bbf62
marcodmc <marcodamascenodev@gmail.com>
2024-12-03 08:32:50 -0300

    chore: reinstalling chrome driver

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from src.app.app import *
+import os
 
 if __name__ == "__main__":
+    DeleteDriverFolder()
+    time.sleep(10)
+
+    os.system("pip uninstall webdriver-manager -y")
+    os.system("pip install webdriver-manager")
+    time.sleep(10)
     RobotClass()
